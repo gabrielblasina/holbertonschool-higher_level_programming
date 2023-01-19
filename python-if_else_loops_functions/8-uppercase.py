6e984bb5a96816189459a594f13bdc47a2f98512
gabrielblasina <5722@holbertonshool.com>
2023-01-19 15:49:08 -0800

    task-8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    is_upper = ''  # This will give the format of str
+    for character in str:
+        if (ord(character) > 96 and ord(character) < 123):
+            is_upper += chr(ord(character) - 32)  # we need to typecast it
+        else:
+            is_upper += character
+    print("{}".format(is_upper))
