96e7f610c25c28fd1c23dceaf2b711b24edaa98a
gabrielblasina <5722@holbertonshool.com>
2023-01-19 16:09:49 -0800

    task-9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number >= 0):
+        print(f"{number % 10}", end='')
+        return (number % 10)
+    if (number < 0):
+        number = number * -1
+        print(f"{number % 10}", end='')
+        return (number % 10)
