b45986fed393a72480eb8ac13f4511b5fadff36d
gabrielblasina <5722@holbertonshool.com>
2023-01-24 08:10:34 -0800

    task-3 correction case my_list = None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_rev_list = my_list[::-1]
-    for number in range(0, len(my_rev_list)):
-        print("{:d}".format(my_rev_list[number]))
+    if (my_list == None):
+        return (None)
+    for number in reversed(my_list):
+        print("{:d}".format(number))
