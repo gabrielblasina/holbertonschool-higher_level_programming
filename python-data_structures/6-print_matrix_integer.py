7451fed747a61563b0bee4b5e01a42e0056bccb2
gabrielblasina <5722@holbertonshool.com>
2023-01-24 10:49:32 -0800

    task-6 print str format correction

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for counter, number in enumerate(rows):
             if (counter == (len(rows) - 1)):
-                print("{}".format(number), end='')
+                print("{:d}".format(number), end='')
             else:
-                print("{}".format(number), end=' ')
+                print("{:d}".format(number), end=' ')
         print()
