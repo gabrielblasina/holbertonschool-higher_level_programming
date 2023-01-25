8aba3735c45d9f9d9f436853f63c7e3f63f654e1
gabrielblasina <5722@holbertonshool.com>
2023-01-25 15:33:58 -0800

    task-9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if (my_list is None):
+        return (None)
+    max_number = my_list[0]
+    for number in my_list:
+        if (max_number < number):
+            max_number = number
+    return (max_number)
