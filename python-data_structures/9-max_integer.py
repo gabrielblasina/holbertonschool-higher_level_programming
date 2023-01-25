a088f5af3ffba7b3023cecc786e6167c82d926f0
gabrielblasina <5722@holbertonshool.com>
2023-01-25 15:41:19 -0800

    task-9 edge case correction

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (my_list is None):
+    if (my_list is None or not my_list):
         return (None)
     max_number = my_list[0]
     for number in my_list:
