bfa2b398c628c3ba5304fc425fbe98a618c3e371
gabrielblasina <5722@holbertonshool.com>
2023-01-25 19:03:47 -0800

    task-10 edge case correction

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     my_new_list = []
-    for index in range(0, (len(my_list) - 1)):
+    for index in range(0, (len(my_list))):
         if (my_list[index] % 2 == 0):
             my_new_list.append(True)
         else:
