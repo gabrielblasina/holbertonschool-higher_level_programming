d2f68cfb4edec261c73207ff22a3410e7183f190
gabrielblasina <5722@holbertonshool.com>
2023-01-26 12:32:44 -0800

    task-0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix1 = []
+    new_matrix2 = []
+    for rows in matrix:
+        new_matrix1 = list(map(lambda value: value ** 2, rows))
+        new_matrix2.append(new_matrix1)
+    return (new_matrix2)
