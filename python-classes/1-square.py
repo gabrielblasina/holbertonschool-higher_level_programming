4434af88d640a1e58f77bfa79e449b8660e0250c
gabrielblasina <5722@holbertonshool.com>
2023-02-02 13:25:38 -0800

    task-1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+No module imported
+"""
+
+
+class Square:
+    """
+    This is a class called Square wich defines a square
+    """
+    def __init__(self, size=0):
+        """
+        __init__ method
+        Args:
+            size: Private atribute
+        """
+        self.__size = size
