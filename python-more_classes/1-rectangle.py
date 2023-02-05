01a5dbfca7cf4a041a814ec4211fbec4ea87260b
gabrielblasina <5722@holbertonshool.com>
2023-02-05 08:15:48 -0800

    task-1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+No module imported
+"""
+
+
+class Rectangle:
+    """
+    Class that defines a Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        __init__ method
+        Args:
+            width: Private atribute
+            height: Private atribute
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        private instance method
+        Note:
+            returns the width
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        private instance method
+        Note:
+            value validation
+        Args:
+            value: private instance width's attribute
+        """
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        private instance method
+        Note:
+            returns the height
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        private instance method
+        Note:
+            value validation
+        Args:
+            value: private instance height's attribute
+        """
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
