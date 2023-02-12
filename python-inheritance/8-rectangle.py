96f698dbd97f88a0ba948d6d49b4c37d75933f24
gabrielblasina <5722@holbertonshool.com>
2023-02-12 04:52:07 -0800

    task-8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """__init__ method
+        Note:
+            integer_validator to validate if args are integers greatter than 0
+        Args:
+            width: (private atribute) width of rectangle
+            height: (private atribute) height of rectangle
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('heigth', height)
+        self.__width = width
+        self.__height = height
