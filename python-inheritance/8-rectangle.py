d0dc3ef115dd10da739558e1a0a0d78eaf37395a
gabrielblasina <5722@holbertonshool.com>
2023-02-12 04:55:26 -0800

    task-8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
             width: (private atribute) width of rectangle
             height: (private atribute) height of rectangle
         """
-        self.integer_validator('width', width)
-        self.integer_validator('heigth', height)
+        self.integer_validator("width", width)
+        self.integer_validator("heigth", height)
         self.__width = width
         self.__height = height
