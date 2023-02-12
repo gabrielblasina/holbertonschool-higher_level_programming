02102322a1a43b27d84366a34569443328d9d39e
gabrielblasina <5722@holbertonshool.com>
2023-02-11 19:07:08 -0800

    task-6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """public instance method
+        Raise:
+            Exception: 'area() is not implemented'
+        """
+        raise Exception('area() is not implemented')
