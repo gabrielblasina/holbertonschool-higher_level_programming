c4ad5585c35e4bf4fb6bee45ed95654963f0ef3d
gabrielblasina <5722@holbertonshool.com>
2023-02-12 16:35:14 -0800

    task-6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""JSON file to object"""
+import json
+
+
+def save_to_json_file(filename):
+    """function to decode a JSON file
+    Args:
+        filename: name of the file
+    """
+    with open(filename, encoding='UTF8') as my_file:
+        new_obj = json.loads(my_file)
+        return (new_obj)
