d6caf1529d1067501525d36158f7c40a4bb9ccd7
gabirle <5722@holbertonstudents.com>
2023-02-16 14:23:11 -0800

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""First Rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """__init__ method
+        Args:
+            width: Privat instance attribute
+            height: Privat instance attribute
+            x: Privat instance attribute
+            y: Privat instance attribute
+            id: Public instance attribute
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
