300fbb68cc893483b94fa97b1729619947b6a318
gabirle <5722@holbertonstudents.com>
2023-02-18 05:00:59 -0800

    implementation of static method in base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """First class Base"""
 
 
+import json
+
+
 class Base:
     """class called Base"""
 
@@ -17,3 +20,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method
+        Args:
+            list_dictionaries: a list of dictionaries
+        Return:
+            JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
