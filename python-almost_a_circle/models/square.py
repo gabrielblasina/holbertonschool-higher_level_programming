60b70284c1ec4c8c014069f8a108920ca2b203dd
gabirle <5722@holbertonstudents.com>
2023-02-17 17:06:09 -0800

    square agrs and kwargs implementation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -49,3 +49,29 @@ class Square(Rectangle):
             string representation of an object
         """
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    def update(self, *args, **kwargs):
+        """Function to update the rectangle
+        Args:
+            *args: no_keyword argument
+            **kwargs: keyword argument
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
