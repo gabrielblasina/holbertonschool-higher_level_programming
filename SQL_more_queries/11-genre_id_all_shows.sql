131af4a287f00b7a6739b4c699fb9fcab5ea884e
gabirle <5722@holbertonstudents.com>
2023-03-18 19:49:47 -0700

    task-11 correction

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- 11. Genre ID for all shows
 -- lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_show.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id;
+SELECT tvs.title, tvsg.genre_id
+FROM tv_shows AS tvs
+LEFT JOIN tv_show_genres AS tvsg
+ON tvs.id = tvsg.show_id
+ORDER BY tvs.title, tvsg.genre_id;
