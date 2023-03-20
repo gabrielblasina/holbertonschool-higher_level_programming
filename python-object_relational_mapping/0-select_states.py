c2819fe9e4fd21adc231a5842190447e3b73ad1d
gabirle <5722@holbertonstudents.com>
2023-03-19 17:30:30 -0700

    task-0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,10 @@ database hbtn_0e_0_usa
 """
 
 
-import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
