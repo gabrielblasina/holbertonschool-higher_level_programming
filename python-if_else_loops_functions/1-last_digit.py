bc57995d83b16b62a2d0554e0004a31daa43919f
gabrielblasina <5722@holbertonstudents.com>
2023-04-10 09:42:45 -0700

    better code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,19 +3,19 @@ import random
 last_number = 0
 number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is", end=" ")
-if (number >= 0):
+if number >= 0:
     last_number = number % 10
-if (number < 0):
+if number < 0:
     last_number = (number * -1) % 10
-if (last_number > 5):
-    if (number > 0):
+if last_number > 5:
+    if number > 0:
         print(f"{last_number} and is greater than 5")
-    if (number < 0):
+    if number < 0:
         print(f"-{last_number} and is less than 6 and not 0")
-elif (last_number < 6 and last_number != 0):
-    if (number > 0):
+elif last_number < 6 and last_number != 0:
+    if number > 0:
         print(f"{last_number} and is less than 6 and not 0")
-    if (number < 0):
+    if number < 0:
         print(f"-{last_number} and is less than 6 and not 0")
 else:
     print(f"{last_number} and is 0")
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     is_upper = ''  # This will give the format of str
     for character in str:
-        if (ord(character) > 96 and ord(character) < 123):
+        if ord(character) > 96 and ord(character) < 123:
             is_upper += chr(ord(character) - 32)  # we need to typecast it
         else:
             is_upper += character
