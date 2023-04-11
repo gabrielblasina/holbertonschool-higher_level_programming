00cae2ee5368370fd17bce256b319883007b3f05
gabrielblasina <5722@holbertonstudents.com>
2023-04-11 09:40:32 -0700

    better code

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-if (__name__ == '__main__'):
+from sys import argv
+if __name__ == '__main__':
     add = 0
-    if ((len(sys.argv) - 1) == 0):
+    if (len(argv) - 1) == 0:
         print("{}".format(add))
     else:
-        for index in range(1, len(sys.argv)):
-            add += int(sys.argv[index])
+        for index in range(1, len(argv)):
+            add += int(argv[index])
         print("{}".format(add))
