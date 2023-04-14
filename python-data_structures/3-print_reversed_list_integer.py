5f34b1b29aa90128239ec06d91b50a545c6ac6b1
gabrielblasina <5722@holbertonstudents.com>
2023-04-14 09:28:34 -0700

    implement of list comprehension task-3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (my_list is None):
-        return (None)
-    for number in reversed(my_list):
-        print("{:d}".format(number))
+    if my_list is None:
+        return None
+    [print('{:d}'.format(n)) for n in reversed(my_list)]
