2804a7af700c5db4a436ccd7af09a7f188981f0e
gabrielblasina <5722@holbertonstudents.com>
2023-04-25 15:35:11 -0700

    improving code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    prev_value = 0
+
     if type(roman_string) != str or roman_string is None:
         return 0
-    for letter in reversed(roman_string):
-        curr_value = rom_dic[letter]
-        if curr_value >= prev_value:
-            result += curr_value
+    for i in range(len(roman_string)):
+        if i > 0 and dic[roman_string[i]] > dic[roman_string[i - 1]]:
+            result += dic[roman_string[i]] - dic[roman_string[i - 1]]
         else:
-            result -= curr_value
-        prev_value = curr_value
+            result += dic[roman_string[i]]
     return result
--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if (a_dictionary.get(key) is None):
-        return (a_dictionary)
+    if a_dictionary.get(key) is None:
+        return a_dictionary
     a_dictionary.pop(key)
-    return (a_dictionary)
+    return a_dictionary
