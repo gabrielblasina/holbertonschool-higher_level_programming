a0642801f9469e504825e5fded77cab55ce2c52d
gabrielblasina <5722@holbertonstudents.com>
2023-04-27 14:20:58 -0700

    task-100code corrections

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -4,10 +4,8 @@ from sys import stderr
 
 def safe_print_integer_err(value):
     try:
-        int_value = int(value)
-    except Exception:
-        print("Exception: Unknown format code 'd' for object of type 'str'",
-              file=stderr)
+        print('{:d}'.format(value))
+    except Exception as e:
+        print('Exception: {}'.format(e), file=stderr)
         return False
-    print("{:d}".format(int_value))
     return True
