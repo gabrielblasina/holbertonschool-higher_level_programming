0faba5cb973a26740dcf605de7e9e2c8e2f86b9e
gabrielblasina <5722@holbertonstudents.com>
2023-06-04 16:00:13 -0700

    code fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from add_0 import add
+from add_0 import add
+if __name__ == "__main__":
     a, b = 1, 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
